504ec05e7bc9f5b6cddd5c7129251497d4364a88
juand <juand@172.22.34.139>
2022-10-27 08:57:03 -0500

    Commit inicial del proyecto - 10272022

---
+++ IsabellaCAMARGOJuanVALDERRAMASebastianVEGASIMON/src/co/edu/unbosque/model/Simon.java
@@ -0,0 +1,54 @@
+package co.edu.unbosque.model;
+
+import java.util.Random;
+
+public class Simon {
+	
+	int [] simondice;
+	
+	public Simon() {
+		simondice = new int [4];
+	}
+
+	public int[] getSimondice() {
+		return simondice;
+	}
+
+	public void generarCodigo () {
+		
+		Random r = new Random();
+		
+		simondice [0] = r.nextInt(5);
+		simondice [1] = r.nextInt(5);
+		simondice [2] = r.nextInt(5);
+		simondice [3] = r.nextInt(5);
+		
+		for (int i = 0; i < simondice.length; i++) {
+			if (simondice[i] == 0) {
+				simondice [i] = r.nextInt(5);
+			}
+		}
+	
+	}
+	
+	public boolean compararNumeros (int [] jugador) {
+		
+		int i = 0;
+		boolean romper = false;
+		while (i < 4 && romper == false) {
+			if (simondice [i] == jugador [i]) {
+				romper = false;
+				i++;
+			} else {
+				romper = true;
+			}
+		}
+		
+		return romper;
+	}
+	
+	
+	
+	
+
+}
